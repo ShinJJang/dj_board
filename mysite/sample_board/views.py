5e1a16db7db14d066545aa3f2c1eac50586723cf
uppertaste <makao.rule@gmail.com>
2013-07-18 04:09:10 +0900

    not search and not korean

--- mysite/sample_board/views.py
+++ mysite/sample_board/views.py
@@ -31,7 +31,7 @@ def show_write_form(request):
 def DoWriteBoard(request):
 	br = DjangoBoard (subject = request.POST['subject'],
 				name = request.POST['name'],
-				mail = request.POST['email'],
+				mail = request.POST['mail'],
 				memo = request.POST['memo'],
 				created_date = timezone.now(),
 				hits = 0,
@@ -77,4 +77,77 @@ def viewWork(request):
 	return render_to_response('viewMemo.html',{'memo_id': request.GET['memo_id'],
 								'current_page': request.GET['current_page'],
 								'searchStr': request.GET['searchStr'],
-								'boardData': boardData})	
+								'boardData': boardData})
+
+def listSearchedSpecificPageWork(request):
+	searchStr = request.GET['searchStr']
+	pageForView = request.GET['pageForView']
+
+	totalCnt = DjangoBoard.objects.filter(subject__contains=searchStr).count()
+
+	pagingHelperIns = pagingHelper();
+	totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+
+	PageIndex = int(pageForView)-1
+	start = int(rowsPerPage*PageIndex)
+
+	boardList = DjangoBoard.objects.raw('SELECT X.* FROM (SELECT * FROM SAMPLE_BOARD_DJANGOBOARD WHERE SUBJECT LIKE \'a\' ) X ORDER BY ID DESC LIMIT %s, %s', [start, rowsPerPage])
+
+	return render_to_response('listSearchedSpecificPage.html', {'boardList': boardList, 'totalCnt': totalCnt,
+								'pageForView': int(pageForView), 'searchStr': searchStr, 'totalPageList': totalPageList})
+
+def listSpecificPageWork_to_update(request):
+	memo_id = request.GET['memo_id']
+	#current_page = request.GET['current_page']
+	#searchStr = request.GET['searchStr']
+	boardData = DjangoBoard.objects.get(id=memo_id)
+	return render_to_response('viewForUpdate.html', {'memo_id':request.GET['memo_id'],
+								'current_page':request.GET['current_page'],
+								'searchStr':request.GET['searchStr'],
+								'boardData':boardData})
+
+@csrf_exempt
+def updateBoard(request):
+	memo_id = request.POST['memo_id']
+	current_page = request.POST['current_page']
+	searchStr = request.POST['searchStr']
+
+	DjangoBoard.objects.filter(id=memo_id).update(
+								mail=request.POST['mail'],
+								subject=request.POST['subject'],
+								memo=request.POST['memo'])
+	# Display Page => POST 요청은 redirection으로 처리
+	url = '/listSpecificPageWork?current_page=' + str(current_page)
+	return HttpResponseRedirect(url)
+
+def DeleteSpecificRow(request):
+	memo_id = request.GET['memo_id']
+	current_page = request.GET['current_page']
+
+	p = DjangoBoard.objects.get(id=memo_id)
+	p.delete()
+
+	# 마지막 메모를 삭제하는 경우, 페이지를 하나 줄임
+	totalCnt = DjangoBoard.objects.all().count()
+	pagingHelperIns = pagingHelper();
+
+	totalPageList = pagingHelperIns.getTotalPageList(totalCnt, rowsPerPage)
+	print 'totalPages', totalPageList
+
+	if(int(current_page) in totalPageList):
+		print 'current_page No Change'
+		current_page = current_page
+	else:
+		current_page = int(current_page) - 1
+		print 'current_page--'
+
+	url = '/listSpecificPageWork?current_page=' + str(current_page)
+	return HttpResponseRedirect(url)
+
+@csrf_exempt
+def searchWithSubject(request):
+	searchStr = request.POST['searchStr']
+	print 'searchStr', searchStr
+
+	url = '/listSearchedSpecificPageWork?searchStr=' + searchStr + '&pageForView=1'
+	return HttpResponseRedirect(url)
